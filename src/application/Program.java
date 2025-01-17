21e9f01c6703cca1609f5354a3c6d52864579c4d
Eldo Lima <eldosilva906@gmail.com>
2025-01-16 23:12:42 -0300

    FindById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,6 +4,8 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,15 +13,12 @@ public class Program {
 
 	public static void main(String[] args){
 		
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		//Date dateB = sdf.parse("27/06/1998");
-
-		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		//
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		//Seller seller1 = new Seller(21, "Eldo", "eldo@gmail.com",dateB ,300.0, obj);
-		Seller seller2 = new Seller(21, "Eldo", "eldo@gmail.com",new Date() ,300.0, obj);
-		System.out.println(seller2);
+		Seller seller = sellerDao.findById(3);
+		System.out.println(seller);
+
 	}
 
 }
